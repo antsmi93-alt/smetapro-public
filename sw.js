25d75c7e43cdff31f021f7afc69d09d3f40c4449
antsmi93-alt <antsmi93@gmail.com>
2026-06-16 13:37:41 +0300

    chore: cache v1.4.9

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 //  Версия кеша: v1.0.0
 // =============================================================
 
-const CACHE_VERSION = 'smeta-pro-v1.4.8';
+const CACHE_VERSION = 'smeta-pro-1.4.9';
 const APP_SHELL = [
   './',
   './calculator-universal.html',
